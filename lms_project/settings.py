d8ea22fbd1d95dda29c2758e10567f01e7d049ac
unknown <kowsalya2025>
2026-01-19 21:56:38 +0530

    Remove sensitive files

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -40,7 +40,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DJANGO_SECRET_KEY='8rt@1h@sgnbd9z4ml10x!ougmx_6iv$3nj90(iw-*mc9c#6z&o'
+SECRET_KEY='django-insecure-l!2jv%k&ogob=b1a)93y(q0h$8$#csjiwikt=oq0y!g!c*&w+l'
 
 
 
