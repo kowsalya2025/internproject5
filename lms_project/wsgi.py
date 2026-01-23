d9cc5aa93bc92b29625060e20563f6480dcdf17b
unknown <kowsalya2025>
2026-01-23 11:45:55 +0530

    Add automatic superuser creation on startup

--- lms_project/wsgi.py
+++ lms_project/wsgi.py
@@ -1,16 +1,26 @@
-"""
-WSGI config for lms_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lms_project.settings')
 
+# Initialize Django
 application = get_wsgi_application()
+
+# --- Automatic superuser creation start ---
+import django
+from django.contrib.auth import get_user_model
+
+django.setup()  # Make sure models and DB are ready
+
+User = get_user_model()
+
+# Only create superuser if it doesn't exist
+if not User.objects.filter(email=os.environ.get("DJANGO_SUPERUSER_EMAIL")).exists():
+    User.objects.create_superuser(
+        username=os.environ.get("DJANGO_SUPERUSER_USERNAME"),
+        email=os.environ.get("DJANGO_SUPERUSER_EMAIL"),
+        password=os.environ.get("DJANGO_SUPERUSER_PASSWORD"),
+    )
+# --- Automatic superuser creation end ---
+
